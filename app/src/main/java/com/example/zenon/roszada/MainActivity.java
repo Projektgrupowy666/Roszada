170ca7282b7cdce65544aa060e8ab458c642018b
timltm666 <timltm666@gmail.com>
2016-03-09 23:07:41 +0100

    Wstawiono layout glownego okna

--- app/src/main/java/com/example/zenon/roszada/MainActivity.java
+++ app/src/main/java/com/example/zenon/roszada/MainActivity.java
@@ -26,7 +26,8 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Projekt grupowy made by Karina Koprowska, Łukasz Rumiński, " +
+                        "Kamil Ślęczkowski.", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
