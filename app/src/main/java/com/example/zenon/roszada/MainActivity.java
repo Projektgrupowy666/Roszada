ec39b212d8f8a8b4431ad763056e92de9a32b71d
timltm666 <timltm666@gmail.com>
2016-03-09 21:18:50 +0100

    test 2 uzytkownika

--- app/src/main/java/com/example/zenon/roszada/MainActivity.java
+++ app/src/main/java/com/example/zenon/roszada/MainActivity.java
@@ -8,7 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-//check1222222222
+//check12
 public class MainActivity extends AppCompatActivity {
 
     @Override
